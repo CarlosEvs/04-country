8e81ec866d05490edf00ef814662b51750eb3c8d
Charlie.estevez <carlos.estevezviejo@plexus.es>
2023-10-06 11:04:32 +0200

    Master

---
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-country-page',
+  templateUrl: './country-page.component.html',
+  styles: [
+  ]
+})
+export class CountryPageComponent implements OnInit{
+
+  constructor ( activatedRoute: ActivatedRoute){}
+
+  ngOnInit(): void {
+
+
+    }
+  }
+
---
+++ src/app/countries/services/countries.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/internal/Observable';
+import { Country } from '../interfaces/country';
+import { catchError, map, of, tap } from 'rxjs';
+
+@Injectable({ providedIn: 'root' })
+export class CountriesService {
+  private apiUrl: string = 'https://restcountries.com/v3.1';
+
+  constructor(private http: HttpClient) {}
+
+  searchCapital(term: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/capital/${term}`;
+
+    return this.http.get<Country[]>(url)
+    .pipe(
+       catchError ( error => of ([]))
+      )
+    ;
+  }
+
+  searchCountry (term:string): Observable<Country[]>{
+
+    const url = `${this.apiUrl}/name/${term}`;
+
+    return this.http.get<Country[]> ( url )
+    .pipe(
+      catchError (error => of ([]))
+    );
+  }
+
+  searchRegion (region:string): Observable<Country[]>{
+
+    const url = `${this.apiUrl}/region/${region}`;
+
+    return this.http.get<Country[]> ( url )
+    .pipe(
+      catchError (error => of ([]))
+    );
+  }
+
+}
